5acb0014f13c20b3005e21362ca7a5bfbcc5e62d
Valentín Praderio <142121934+valendotjpg@users.noreply.github.com>
2026-05-15 20:57:55 -0300

    Implement basic calculator functions and menu

---
+++ calculadora.py
@@ -0,0 +1,25 @@
+salir = 0
+def menu():
+  while salir = 0:
+  operacion = input("Ingresa la operacion que quieras realizar: ")
+  print("" 
+        " 1 - Suma
+        " 2 - Resta
+        " 3 - Multiplicacion
+        
+        
+        "")
+
+
+        
+def suma(x,y):
+  suma = x + y
+  return suma
+
+def resta(x,y):
+  resta = x - y
+  return resta
+  
+def multiplicacion(x,y):
+  mult = x . y
+  
